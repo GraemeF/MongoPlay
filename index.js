6bae4364a2aa4f546b153452fa89c9c6d483267d
Graeme Foster <graemef@gmail.com>
2013-01-03 15:58:40 +0000

    Wait for the insert to complete once per 3000 inserts

--- index.js
+++ index.js
@@ -9,25 +9,58 @@ var devices = 70000;
 var packages = 50;
 var files = 60;
 
-var insertEverything = function(err, collection) {
-    for (var device = 0; device < devices; device++) {
-      for (var packageNo = 0; packageNo < packages; packageNo++) {
-        for (var file = 0; file < files; file++) {
-          collection.insert({
-            device: 'device ' + device,
-            package: {
-              name: 'package ' + packageNo
-            },
-            file: {
-              name: 'some file' + file,
-              version: '1.0.0.0'
-            }
-          });
+var insertDevice = function(device, collection, callback) {
+    console.log('inserting device ' + device);
+    for (var packageNo = 1; packageNo <= packages; packageNo++) {
+      for (var file = 1; file <= files; file++) {
+        var options;
+        var cb;
+
+        if (file === files && packageNo === packages) {
+          options = {
+            safe: true
+          };
+          cb = callback;
+        } else {
+          options = {
+            safe: false
+          };
+          cb = function(err) {
+            if (err) throw err;
+          }
         }
+
+        collection.insert({
+          device: 'device ' + device,
+          package: {
+            name: 'package ' + packageNo
+          },
+          file: {
+            name: 'some file' + file,
+            version: '1.0.0.0'
+          }
+        }, options, cb);
       }
-      console.log('device ' + device);
     }
-    client.close();
+  };
+
+var insertEverything = function(err, collection) {
+    function doneDevice(err, result) {
+      //console.log(JSON.stringify(result));
+      var device = parseInt(result[0].device.substr(6));
+      process.nextTick(function() {
+        //console.log('called back ' + device + '/' + devices);
+        if (device < devices) {
+          insertDevice(++device, collection, doneDevice);
+          console.log('device ' + device);
+        } else {
+          console.log('done');
+          client.close();
+        }
+      });
+    };
+
+    insertDevice(1, collection, doneDevice);
   }
 
 client.open(function(err, p_client) {
