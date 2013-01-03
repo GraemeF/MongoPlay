9faaa407d42d5034f2a626b0b428b0c48d225d26
Graeme Foster <graemef@gmail.com>
2013-01-03 14:08:28 +0000

    Insert a collection of device/package/file combinations

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 var mongodb = require('mongodb');
 require('chai').should();
 
-var client = new mongodb.Db('millions', new mongodb.Server("127.0.0.1", 27017, {}), {
-  w: 1
+var client = new mongodb.Db('millions', new mongodb.Server("192.168.0.162", 27017, {}), {
+  w: 0
 });
-var test = function(err, collection) {
-    collection.insert({
-      a: 2
-    }, function(err, docs) {
 
-      collection.count(function(err, count) {
-        count.should.equal(1);
-      });
+var devices = 70000;
+var packages = 50;
+var files = 60;
 
-      // Locate all the entries using find
-      collection.find().toArray(function(err, results) {
-        results.length.should.equal(1);
-        results[0].a.should.equal(2);
-
-        // Let's close the db
-        client.close();
-      });
-    });
-  };
+var insertEverything = function(err, collection) {
+    for (var device = 0; device < devices; device++) {
+      for (var packageNo = 0; packageNo < packages; packageNo++) {
+        for (var file = 0; file < files; file++) {
+          collection.insert({
+            device: 'device ' + device,
+            package: {
+              name: 'package ' + packageNo
+            },
+            file: {
+              name: 'some file' + file,
+              version: '1.0.0.0'
+            }
+          });
+        }
+      }
+      console.log('device ' + device);
+    }
+    client.close();
+  }
 
 client.open(function(err, p_client) {
-  client.collection('sccm_inventory', test);
+  client.collection('sccm_inventory', insertEverything);
 });
